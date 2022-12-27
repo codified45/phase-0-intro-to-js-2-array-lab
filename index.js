116346213d207d873b14573e816478d33cd874c1
Jack Lach <jack45k@gmail.com>
2022-12-26 19:37:40 -0500

    commit tree c212f03c032eb5277f01405273975590ba4db6d0

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+
+function appendCat(name) {
+    const newAppendedCats = [...cats, name];
+    return newAppendedCats;
+}
+
+
+function prependCat(name) {
+    const newPrependedCats = [name, ...cats];
+    return newPrependedCats;
+}
+
+function removeLastCat() {
+    const newRemovedLastInCats = cats.slice(0, cats.length - 1);
+    return newRemovedLastInCats;
+}
+
+function removeFirstCat() {
+    const newRemovedFirstInCats = cats.slice(1);
+    return newRemovedFirstInCats;
+}
+
+
+
