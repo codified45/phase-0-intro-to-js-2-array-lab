bf65a5a65308cbeb3786e821a3d6d27fa0eb359f
Jack Lach <jack45k@gmail.com>
2023-01-22 03:20:02 -0500

    array lab, all 9 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat = function(name){
+cats.push(name);
+}
+
+const destructivelyPrependCat = function(name){
+cats.unshift(name);
+}
+
+const destructivelyRemoveLastCat = function(){
+cats.pop();
+}
+
+const destructivelyRemoveFirstCat = function(){
+cats.shift();
+}
+
+const appendCat = function(name){
+const newCats = [...cats, name];
+return newCats;
+}
+
+const prependCat = function(name){
+const newCats = [name, ...cats];
+return newCats;
+}
+
+const removeLastCat = function(){
+const newCats = cats.slice(0, -1);
+return newCats;
+}
+
+const removeFirstCat = function(){
+const newCats = cats.slice(1);
+return newCats;
+}
